72db8f7a322e9fcebbc58cfc27f71d3d7d4ad6c2
Saturnist <jlau181@gmail.com>
2017-04-09 15:41:29 -0700

    added comments to explain functions

--- prices/bitcoin_calc.py
+++ prices/bitcoin_calc.py
@@ -3,8 +3,13 @@ from datetime import datetime
 
 from coindesk import btc_interval
 
-#shows a dataframe of price on a interval of dates 
 def bpi_df(start, end):
+    """
+    shows a dataframe of price on a interval of dates 
+    :param start: string in year, month, day e.g. 20170419
+    :param end: string in year, mont, day e.g. 20170420
+    :return: dataframe with date and USD column
+    """
     btc = btc_interval(start, end)
     btc = btc['bpi']
     df = pd.DataFrame(list(btc.items()), columns=['Date', 'USD'])
@@ -12,26 +17,52 @@ def bpi_df(start, end):
 
 
 def percent_change(df):
+    """
+    
+    :param df: dataframe from bpi_df
+    :return: amount of percent change in each day following previous day
+    """
     return df['USD'].pct_change()
 
 
 def merger(df, df2):
+    """
+    
+    :param df: data frame from bpi_df
+    :param df2: percent change df
+    :return: merged df
+    """
     merge_df = df.merge(df2.to_frame(), left_index=True, right_index=True)
     merge_df.columns = ['Date', 'USD', 'percent_change']
     return merge_df
 
 
 def high_percents(merge_df):
+    """
+    
+    :param merge_df: merged df
+    :return: shows dates with greater than 9% change
+    """
     date_inc = merge_df[merge_df['percent_change'] > 0.09]
     return date_inc['Date']
 
 
 def low_percents(merge_df):
+    """
+    
+    :param merge_df: merged df
+    :return: shows dates with less than 9% change
+    """
     date_dec = merge_df[merge_df['percent_change'] < -0.09]
     return date_dec['Date']
 
 
 def timestamp_convert(df):
+    """
+    
+    :param df: merged df
+    :return: converts the dates from above into usable timestamps for nyt api
+    """
     times = {'high': [], 'low': []}
 
     for high in high_percents(df):
